90b5de574d41df552f73e48ffbd8f0a63803ff22
Dear-Toshal16 <toshaldeshmukh3@gmail.com>
2025-11-06 21:58:59 +0530

    updated

--- script.js
+++
@@ -1,5 +0,0 @@
-// ======= ADD SIMPLE SCROLL EFFECT TO NAVBAR =======
-window.addEventListener("scroll", () => {
-  const navbar = document.querySelector(".navbar");
-  navbar.classList.toggle("scrolled", window.scrollY > 50);
-});
